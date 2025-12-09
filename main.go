0f3fd407d788d313690724205735ff030bb5a0e6
liam.karv <karvelasliam@gmail.com>
2025-12-09 21:38:20 +1100

    Made todo struct and todos

--- main.go
+++ main.go
@@ -1,2 +1,13 @@
 package main
 
+type todo struct {
+	ID string
+	Item string
+	Completed bool
+}
+
+var todos = []todo {
+	{ID: "1", Item: "Clean Room", Completed: false},
+	{ID: "2", Item: "Break my balls", Completed: false},
+	{ID: "3", Item: "Breathe", Completed: false},
+}
