62c83cd4a66e22904af21bad77707e479d01b5eb
liam.karv <karvelasliam@gmail.com>
2025-12-09 21:44:40 +1100

    Created main function, writing getTodos

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
-type todo struct {
-	ID string
-	Item string
-	Completed bool
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// Client and server communicate through JSON
+type todo struct{
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Completed bool   `json:"completed"`
 }
 
-var todos = []todo {
+var todos = []todo{
 	{ID: "1", Item: "Clean Room", Completed: false},
 	{ID: "2", Item: "Break my balls", Completed: false},
 	{ID: "3", Item: "Breathe", Completed: false},
+}
+
+func getTodos(context *gin.Context){
+
+}
+
+func main(){
+	router := gin.Default()
+	router.GET("/todos")
+	router.Run("localhost:8080")
 }
